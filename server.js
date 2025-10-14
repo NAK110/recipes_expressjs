731c8d7a1d95ac3232328dc0175b2b18c90c6e09
NAK110 <vathanak110@gmail.com>
2025-10-14 09:24:18 +0700

    fix port mismatch in Railway

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ dotenv.config();
 // connectDB();
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: ['http://localhost:5173']
+    origin: '*'
 }
 
 app.use(cors(corsOptions))
